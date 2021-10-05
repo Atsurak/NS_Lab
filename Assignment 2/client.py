0cbf59404949b6d830977bc195f851be8a90b277
Kathuri Abhinav <kathuriabhinav@gmail.com>
2021-10-05 22:38:12 +0530

    Update client.py

--- Assignment 2/client.py
+++ Assignment 2/client.py
@@ -2,7 +2,6 @@
 import socket            
 import hashlib 
 
-
 #defining a hashfunction sha256.
 def hash(n,password):
     temp=password
@@ -12,51 +11,49 @@ def hash(n,password):
         # print(temp)
     return temp
 
-
 # Creating a socket
 s = socket.socket()        
 
-
 # defing the port number
-port = 12350   
-    
+port = 3000 
     
 # connecting to the server(local)
 s.connect(('127.0.0.1', port))
 
-
 #taking username 
-username=input("Enter username:")
-
-
+username=input("Enter username : ")
 
 #sending a username to server
 s.send(username.encode())
 
-
-
 # receive number from server
-# print (s.recv(1024).decode())
-rec=s.recv(1024).decode()
-print(rec)
-rec=int(rec)
-
-
-#taking password from the user
-password=input("\nEnter password:")
-
-#hashing the password
-msg=hash(rec-1,password) 
-
-#sending the hashed password
-s.send(msg.encode())
-
-
-#receive messege from client
 rec=s.recv(1024).decode()
 print(rec)
 
-# close the connection
-s.close()    
+if rec=='User Not Found':
+    s.close() 
+else:
+    rec=int(rec)
+    if rec==0:
+        print("Access Denied!")
+        s.close()
+
+    else:
+        #taking password from the user
+        password=input("\nEnter Password : ")
+
+        #hashing the password
+        msg=hash(rec-1,password) 
+
+        #sending the hashed password
+        s.send(msg.encode())
+
+        #receive messege from client
+        rec=s.recv(1024).decode()
+        print(rec)
+
+        # close the connection
+        s.close()    
+print("Connection with server closed")
     
      
